24385466e208529b6e6acfd4eb14cda015eaf7a3
Charlie Henderson <charlie.michael.henderson@gmail.com>
2023-12-02 11:07:37 +0000

    2nd solution finished for the second day took about 40 mins XD

--- day_2_solution.py
+++ day_2_solution.py
@@ -7,11 +7,15 @@ param_blue = 14
 
 id = 0
 sum_of_ids = 0
+total_power = 0
 for line in input.splitlines():
     id += 1 
     split_line = line.split(": ")
     games = split_line[1].split("; ")
     fails = 0
+    max_red = 0
+    max_green = 0
+    max_blue = 0
     for game in games:
         red = 0
         green = 0
@@ -21,16 +25,25 @@ for line in input.splitlines():
             amount = int(cube.split(" ")[0])
             if cube.find("red") != -1:
                 red = amount
+                if amount > max_red:
+                    max_red = amount
             elif cube.find("green") != -1:
                 green = amount
+                if amount > max_green:
+                    max_green = amount
             elif cube.find("blue") != -1:
                 blue = amount
+                if amount > max_blue:
+                    max_blue = amount
+        
+        
         if (red > param_red or green > param_green or blue > param_blue):
             fails += 1
+    total_power += max_red * max_green * max_blue
     if (fails == 0):
         sum_of_ids += id
 
-print(sum_of_ids)
+print(total_power)
 
 
 
